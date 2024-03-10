d381ce342d5d795d167fea437c64ab07cd5083ec
JoseFabela <jfabela522@gmail.com>
2024-03-10 17:55:15 -0600

    Agregar archivos de proyecto.

---
+++ Ventas ACT4/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Ventas_ACT4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnCargarArchivo = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnShowTable = new System.Windows.Forms.Button();
+            this.btnShowGraph = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCargarArchivo
+            // 
+            this.btnCargarArchivo.Location = new System.Drawing.Point(48, 50);
+            this.btnCargarArchivo.Name = "btnCargarArchivo";
+            this.btnCargarArchivo.Size = new System.Drawing.Size(110, 23);
+            this.btnCargarArchivo.TabIndex = 0;
+            this.btnCargarArchivo.Text = "Upload";
+            this.btnCargarArchivo.UseVisualStyleBackColor = true;
+            this.btnCargarArchivo.Click += new System.EventHandler(this.btnCargarArchivo_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(391, 72);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(587, 386);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(417, 72);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(544, 386);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // btnShowTable
+            // 
+            this.btnShowTable.Location = new System.Drawing.Point(48, 105);
+            this.btnShowTable.Name = "btnShowTable";
+            this.btnShowTable.Size = new System.Drawing.Size(110, 23);
+            this.btnShowTable.TabIndex = 3;
+            this.btnShowTable.Text = "Show Table";
+            this.btnShowTable.UseVisualStyleBackColor = true;
+            this.btnShowTable.Click += new System.EventHandler(this.btnShowTable_Click);
+            // 
+            // btnShowGraph
+            // 
+            this.btnShowGraph.Location = new System.Drawing.Point(48, 151);
+            this.btnShowGraph.Name = "btnShowGraph";
+            this.btnShowGraph.Size = new System.Drawing.Size(110, 23);
+            this.btnShowGraph.TabIndex = 4;
+            this.btnShowGraph.Text = "Show Graph";
+            this.btnShowGraph.UseVisualStyleBackColor = true;
+            this.btnShowGraph.Click += new System.EventHandler(this.btnShowGraph_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(164, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 37);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Double Click For Show The Items Most Selled";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1203, 638);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnShowGraph);
+            this.Controls.Add(this.btnShowTable);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnCargarArchivo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCargarArchivo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnShowTable;
+        private System.Windows.Forms.Button btnShowGraph;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Ventas ACT4/Form1.cs
@@ -0,0 +1,323 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Windows.Forms.DataVisualization;
+using System.Windows.Forms.DataVisualization.Charting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Data;
+using Newtonsoft.Json;
+using System.Xml;
+using System.Reflection;
+
+namespace Ventas_ACT4
+{
+    public partial class Form1 : Form
+    {
+        private DataTable dataTable = new DataTable();
+        private Dictionary<string, int> ventasPorCategoria = new Dictionary<string, int>();
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            ConfigurarChart();
+
+        }
+        private void ConfigurarChart()
+        {
+            
+                // Configurar Chart
+                chart1.ChartAreas.Add("ChartArea");
+                chart1.Series.Add("VentasPorCategoria");
+                chart1.Series["VentasPorCategoria"].ChartType = SeriesChartType.Column;
+                chart1.Titles.Add("Ventas por Categoría");
+
+                // Establecer un tamaño personalizado
+                chart1.Width = 600;
+                chart1.Height = 400;
+            
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Configurar DataGridView
+            dataGridView1.DataSource = dataTable;
+
+            dataGridView1.Visible = false;
+        chart1.Visible = false;
+            // Configurar gráfica (puedes usar un control Chart)
+          
+            dataGridView1.Refresh();
+        }
+
+       
+
+        private void btnCargarArchivo_Click(object sender, EventArgs e)
+        {
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Archivos CSV (*.csv)|*.csv|Archivos XML (*.xml)|*.xml|Archivos JSON (*.json)|*.json|Todos los archivos (*.*)|*.*";
+                openFileDialog.Multiselect = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string filePath in openFileDialog.FileNames)
+                    {
+                        ProcesarArchivo(filePath);
+                    }
+                }
+                MostrarDatosEnGrafica(); // Llamada al método después de cargar los datos
+
+            }
+        }
+        private void ProcesarArchivo(string filePath)
+        {
+            string extension = Path.GetExtension(filePath);
+
+            switch (extension.ToLower())
+            {
+                case ".csv":
+                    CargarCSV(filePath);
+                    break;
+                case ".xml":
+                    CargarXML(filePath);
+                    break;
+                case ".json":
+                    CargarJSON(filePath);
+                    break;
+                default:
+                    MessageBox.Show("Formato de archivo no compatible: " + extension, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
+        }
+
+        private void CargarCSV(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            if (lines.Length > 0)
+            {
+                string[] headers = lines[0].Split(',');
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] data = lines[i].Split(',');
+
+                    // Asignar nombres de columnas basados en la primera línea
+                    if (dataTable.Columns.Count == 0)
+                    {
+                        foreach (string header in headers)
+                        {
+                            dataTable.Columns.Add(header);
+                        }
+                    }
+
+                    // Agregar datos al DataTable
+                    dataTable.Rows.Add(data);
+
+                    // Actualizar ventasPorCategoria
+                    string categoria = data[1]; // Suponiendo que la categoría está en la segunda columna
+                    int ventas = int.Parse(data[3]); // Suponiendo que las ventas están en la cuarta columna
+
+                    if (ventasPorCategoria.ContainsKey(categoria))
+                    {
+                        ventasPorCategoria[categoria] += ventas;
+                    }
+                    else
+                    {
+                        ventasPorCategoria.Add(categoria, ventas);
+                    }
+                }
+            }
+        }
+
+
+        private void CargarXML(string filePath)
+        {
+            // Lógica para cargar datos XML en el DataTable
+            // Ejemplo básico:
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(filePath);
+
+            // Aquí debes adaptar la lógica según la estructura de tu XML
+            // Puedes usar XPath para seleccionar nodos específicos
+        }
+
+
+        public void CargarJSON(string filePath)
+        {
+            try
+            {
+                // Lógica para cargar datos JSON en el DataTable
+                string jsonContent = File.ReadAllText(filePath);
+                List<EstructuraDeJson> data = JsonConvert.DeserializeObject<List<EstructuraDeJson>>(jsonContent);
+
+                if (data.Count > 0)
+                {
+                    dataTable.Clear();
+                    dataTable.Columns.Clear();
+
+                    // Asignar nombres de columnas y tipos basados en propiedades de TuClase
+                    PropertyInfo[] propiedades = typeof(EstructuraDeJson).GetProperties();
+
+                    foreach (PropertyInfo propiedad in propiedades)
+                    {
+                        dataTable.Columns.Add(propiedad.Name, propiedad.PropertyType);
+                    }
+
+                    // Agregar datos al DataTable
+                    foreach (EstructuraDeJson item in data)
+                    {
+                        DataRow row = dataTable.NewRow();
+
+                        foreach (PropertyInfo propiedad in propiedades)
+                        {
+                            row[propiedad.Name] = propiedad.GetValue(item);
+                        }
+
+                        dataTable.Rows.Add(row);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al cargar datos desde JSON: {ex.Message}");
+            }
+        }
+
+        private void MostrarDatosEnGrafica()
+        {
+            // Limpiar datos anteriores en el gráfico
+            chart1.Series["VentasPorCategoria"].Points.Clear();
+
+            // Limpiar títulos existentes antes de agregar uno nuevo
+            chart1.Titles.Clear();
+
+            // Agregar un nuevo título
+            Title chartTitle = new Title("Ventas por Categoría");
+            chartTitle.Font = new Font("Arial", 16, FontStyle.Bold); // Aumenta el tamaño del título
+            chart1.Titles.Add(chartTitle);
+
+            // Configurar la gráfica
+            chart1.Series["VentasPorCategoria"].IsValueShownAsLabel = true;
+
+            foreach (var kvp in ventasPorCategoria)
+            {
+                // Agregar puntos con colores distintos para cada categoría
+                chart1.Series["VentasPorCategoria"].Points.AddXY(kvp.Key, kvp.Value);
+                chart1.Series["VentasPorCategoria"].Points.Last().Color = ObtenerColorPorCategoria(kvp.Key);
+                chart1.Series["VentasPorCategoria"].Points.Last().Label = $"{kvp.Key}\n{String.Format("{0:C}", kvp.Value)}"; // Incluir el nombre de la categoría en la etiqueta
+            }
+        }
+
+
+
+        private Color ObtenerColorPorCategoria(string categoria)
+        {
+            // Asignar colores diferentes según la categoría
+            switch (categoria)
+            {
+                case "Moda":
+                    return Color.Blue;
+                case "Calzado":
+                    return Color.Green;
+                case "Accesorio":
+                    return Color.Orange;
+                case "Deporte":
+                    return Color.Red;
+                default:
+                    return Color.Gray;
+            }
+        }
+        private void EstablecerEstilosTabla()
+        {
+            // Iterar a través de las filas y aplicar estilos según ciertos criterios
+            foreach (DataGridViewRow fila in dataGridView1.Rows)
+            {
+                // Supongamos que la columna 3 representa las ventas
+                int ventas = Convert.ToInt32(fila.Cells[3].Value);
+
+                // Establecer estilos para resaltar ventas altas
+                if (ventas > 100)
+                {
+                    fila.DefaultCellStyle.BackColor = Color.LightGreen;  // Fondo de la fila
+                    fila.DefaultCellStyle.ForeColor = Color.Black;       // Texto de la fila
+                    fila.DefaultCellStyle.Font = new Font(dataGridView1.Font, FontStyle.Bold); // Texto en negrita
+                }
+            }
+        }
+        private void OrdenarTablaPorVentas()
+        {
+            if (dataGridView1.Columns.Count > 3)
+            {
+                dataGridView1.Sort(dataGridView1.Columns[3], ListSortDirection.Descending);
+            }
+
+        }
+        private void ResaltarCeldas()
+        {
+            // Supongamos que la columna 3 representa las ventas
+            foreach (DataGridViewRow fila in dataGridView1.Rows)
+            {
+                int ventas = Convert.ToInt32(fila.Cells[3].Value);
+
+                // Resaltar la celda si las ventas son mayores a 100
+                if (ventas > 100)
+                {
+                    fila.Cells[3].Style.BackColor = Color.LightGreen;
+                    fila.Cells[3].Style.ForeColor = Color.Black;
+                    fila.Cells[3].Style.Font = new Font(dataGridView1.Font, FontStyle.Bold);
+                }
+            }
+        }
+
+
+
+        private void btnShowTable_Click(object sender, EventArgs e)
+        {
+            EstablecerEstilosTabla();
+            ResaltarCeldas();
+            OrdenarTablaPorVentas();
+            dataGridView1.Refresh();
+
+            chart1.Visible = false;
+
+            dataGridView1.Visible = true;
+       
+        }
+
+        private void btnShowGraph_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Visible = false;
+
+            chart1.Visible = true;
+
+        }
+    }
+}
+public class EstructuraDeJson
+{
+    // Define las propiedades según la estructura de tus datos JSON
+    public string Nombre { get; set; }
+    public string Categoria { get; set; }
+
+    public int Precio { get; set; } 
+    public int Ventas { get; set; }
+}
